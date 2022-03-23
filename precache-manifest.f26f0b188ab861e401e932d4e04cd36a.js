2aa5e2cde1f141c3930cfb85ff2484e161017f15
Vladon79 <vla3ik@gmail.com>
2022-03-23 15:32:39 +0300

    Updates

---
+++ precache-manifest.f26f0b188ab861e401e932d4e04cd36a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "531147bd39749bbbc9e13793a7c4ad5d",
+    "url": "/Todolist/index.html"
+  },
+  {
+    "revision": "66e89a2909965b5accbf",
+    "url": "/Todolist/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "379eb3b16c2b5ee6b8bf",
+    "url": "/Todolist/static/js/2.35925b64.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/Todolist/static/js/2.35925b64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66e89a2909965b5accbf",
+    "url": "/Todolist/static/js/main.6d233723.chunk.js"
+  },
+  {
+    "revision": "8c9ba947015c2dd63600",
+    "url": "/Todolist/static/js/runtime-main.e8babc40.js"
+  }
+]);
