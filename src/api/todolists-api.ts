c6129c6be41e8b1978ba5290c8979fe4e5812046
Vladon79 <vla3ik@gmail.com>
2022-03-23 15:31:43 +0300

    deploy

--- src/api/todolists-api.ts
+++ src/api/todolists-api.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 const settings = {
     withCredentials: true,
     headers: {
-        'API-KEY': '1cdd9f77-c60e-4af5-b194-659e4ebd5d41'
+        'API-KEY': '89087f1a-df84-4609-ac91-9f089fdba243'
     }
 }
 const instance = axios.create({
